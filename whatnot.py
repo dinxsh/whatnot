848fa04609b4ea9b9a45f15ff5f50c0cfd851b5e
dinxsh <16219421127@atmiyauni.edu.in>
2022-11-13 23:23:29 +0530

    updated

--- whatnot.py
+++ whatnot.py
@@ -16,56 +16,55 @@ bot  =  commands.Bot(command_prefix=".",  intents=intents)
 bot.remove_command('help')
 
 async def checkforlive():
-    print("here 1")
     whatnot_live = await getwhatnotlive()   
+    print(whatnot_live)
     for live in whatnot_live:
-        id=live.id
-        with open('whatnot.json', 'r+') as f:
-            data = json.load(f)
-            if data['lastvideo']==id:
-                print("here 2")
-            else:
-                print("here 1")
-                data['lastvideo']=live.id
-                f.seek(0)
-                f.write(json.dumps(data))
-                f.truncate()
-                name = live.title
-                views = live.active_viewers
-                categories = live.categories
-                trailer = live.trailer_url
-                embedvar1 = nextcord.Embed(title=str(name) + " | Whatnot", type="rich", description="**" + str(live.user)[7:22] + "**" + " is now live with " + str(views) + f" views. watch it (here)[{[str(trailer)]}].", timestamp=datetime.utcnow(), color=0x5865F2)
-                embedvar1.add_field(name='Categorie\'s', value=','.join(categories))
-                channel = bot.get_channel(1022070373187067965)
-                await channel.send(embed=embedvar1)
+        print(live)
+    id=live.id
+    with open('whatnot.json', 'r+') as f:
+        data = json.load(f)
+        if data['lastvideo']==id:
+            print("here 3")
+        else:
+            print("here 4")
+            data['lastvideo']=live.id
+            f.seek(0)
+            f.write(json.dumps(data))
+            f.truncate()
+            name = live.title
+            views = live.active_viewers
+            embedvar1 = nextcord.Embed(title="{} | Whatnot".format(name), type="rich", description="Upcoming show on {}, **{}**  is now live with {}  views. watch it (here)[{}]".format(live.start_time.utcnow(), str(live.user)[7:22], views, live.trailer_url), timestamp=datetime.utcnow(), color=0x5865F2)
+            channel = bot.get_channel(1022070373187067965)
+            await channel.send(embed=embedvar1)
 
 @bot.event
-async def on_ready( ):
+async def on_ready():
     print("started bot")
-    @tasks.loop(seconds=10)
+    @tasks.loop(minutes=2)
     async def main():
         await checkforlive()
     await main.start()
 
-async def getwhatnotacc( ):
+async def whatnotcon():
     async with Whatnot() as whatnot:
-            await whatnot.login("dineshtalwadker@gmail.com", "Omshanti@2005")
-            whatnot_user = await whatnot.get_user_by_id("1246084")
-            return whatnot_user
+        await whatnot.login("", "")
+        return whatnot
 
+async def getwhatnotacc():
+    whatnot1 = await whatnotcon()
+    whatnot_user = await whatnot1.get_user_by_id("1246084")
+    return whatnot_user
 
-async def getwhatnotlive( ):
-    async with Whatnot() as whatnot:
-        await whatnot.login("dineshtalwadker@gmail.com", "Omshanti@2005")
-        whatnot_user = await whatnot.get_user_by_id("1246084")
-        lives = await whatnot.get_user_lives(whatnot_user.id)
-        return lives
+async def getwhatnotlive():
+    whatnot1 = await whatnotcon()
+    lives = await whatnot1.get_user_lives("1246084")
+    return lives
 
 @bot.command()
 async def whatnot(ctx):
     whatnot_user = await getwhatnotacc()
-    embedvar = nextcord.Embed(title="[" + str(whatnot_user.username) + f"]{whatnot_user.profile_url} | whatnot \u200B", timestamp=datetime.utcnow(), description="**about me:**\n" + str(whatnot_user.bio) + "\n", color=0x5865F2)
-    embedvar.set_thumbnail(whatnot_user.profile_image)
+    embedvar = nextcord.Embed(title= "[{}]({})".format(whatnot_user.username) + " | whatnot \u200B", timestamp=datetime.utcnow(), description="**about me:**\n" + str(whatnot_user.bio) + "\n", color=0x5865F2)
+    embedvar.set_thumbnail(whatnot_user.profile_url)
     embedvar.set_footer(text='\u200b',icon_url="https://i.imgur.com/uZIlRnK.png")
     embedvar.add_field(name='Seller Ratings', value=str(whatnot_user.seller_rating["numReviews"])+ ", Overall Rating: " + str(whatnot_user.seller_rating["overall"]))
     embedvar.add_field(name='Followers', value=whatnot_user.follower_count)
@@ -94,7 +93,7 @@ async def game(ctx, *, game):
         embedvar = nextcord.Embed(title=a["games"][0]["name"], timestamp=datetime.utcnow(), description="now available for " + price + " " + currency, color=0x5865F2)
         embedvar.set_footer(text='\u200b',icon_url="https://i.imgur.com/uZIlRnK.png")
         embedvar.add_field(name='Release Date', value=a["games"][0]["releaseDate"], inline=True)
-        embedvar.add_field(name='Stores', value="available at [" + str(a["games"][0]["stores"][0]['seller']) + "](" + a["games"][0]["stores"][0]['url']+")", inline=True)
+        embedvar.add_field(name='Stores', value="available at [{}]({})".format(str(a["games"][0]["stores"][0]['seller']), a["games"][0]["stores"][0]['url']+")", inline=True))
         print("--- %s seconds ---" % (time.time() - start_time))
         await ctx.reply(embed=embedvar)
         await sess.close()
